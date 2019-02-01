4140b098c40506222f88e5fa5686f9f75525721e
kunal_gaba <kunalgaba44@gmail.com>
2019-01-31 21:07:31 -0500

    Removed Uneccessary methods.

--- src/com/lambton/magicalCardGame/MagicMain.java
+++ src/com/lambton/magicalCardGame/MagicMain.java
@@ -54,32 +54,6 @@ public class MagicMain {
         }
     }
 
-    private static void column1(String[][] cardList) {
-        String transpose[][] = new String[3][3];
-        for (int i = 0; i < cardList.length; i++) {
-            for (int j = 0; j < cardList[i].length; j++) {
-                transpose[i][j] = cardList[j][i];
-            }
-        }
-        displayMatrix(transpose);
-        interchangeColumns(transpose);
-    }
-
-    private static void interchangeColumns(String[][] transpose) {
-
-        //x for 1 y for 2
-        int x = 1;
-        int y = 2;
-        String temp;
-        String[][] tranposedMatrix = transpose;
-        for (int i = 0; i < 3; i++) {
-            temp = tranposedMatrix[(x - 1)][i];
-            tranposedMatrix[x - 1][i] = tranposedMatrix[y - 1][i];
-            tranposedMatrix[y - 1][i] = temp;
-        }
-        displayMatrix(tranposedMatrix);
-    }
-
     private static void displayMatrix(String[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
