b1f4f9a81ae05d9e9e69527801dac93863d7f2a7
kunal_gaba <kunalgaba44@gmail.com>
2019-01-31 19:44:04 -0500

    Added code for column alterations

--- src/com/lambton/magicalCardGame/MagicMain.java
+++ src/com/lambton/magicalCardGame/MagicMain.java
@@ -13,6 +13,35 @@ public class MagicMain {
         System.out.println("Enter the column which contains your card");
         magicalCardGameModel.setFirstShuffleColPos(sc.next());
 
+        column1(magicalCardGameModel.getCardList());
+
+    }
+
+    private static void column1(String[][] cardList) {
+        String transpose[][] = new String[3][3];
+        for (int i = 0; i < cardList.length; i++) {
+            for (int j = 0; j < cardList[i].length; j++) {
+                transpose[i][j] = cardList[j][i];
+            }
+        }
+        interchangeColumns(transpose);
+        displayMatrix(transpose);
+    }
+
+    private static void interchangeColumns(String[][] transpose) {
+        
+        //x for 1 y for 2
+        int x=1;
+        int y=2;
+        String temp;
+        String[][] tranposedMatrix= transpose;
+        for(int i = 0; i < 3; i++)
+        {
+            temp = tranposedMatrix[i][(x-1)];
+            tranposedMatrix[i][x-1] = transpose[i][(y-1)];
+            tranposedMatrix[i][y-1] = temp;
+        }
+        displayMatrix(tranposedMatrix);
     }
 
     private static void displayMatrix(String[][] matrix) {
